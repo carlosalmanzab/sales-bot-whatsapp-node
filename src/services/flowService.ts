b4dbf8f052233c5afcb45fa7562b4599ff38bd6f
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-05-17 13:42:40 -0500

    feat: add mark read to chat flow

--- src/services/flowService.ts
+++ src/services/flowService.ts
@@ -1,11 +1,11 @@
+import { markRead } from "../util/actions/actionMessage";
 import { buildTextMessage } from "../util/builders/sendMessageBuilders";
 
 export const chatflow = (message: string, phoneNumber: string , messageId: string , name: string) => {
     const list = [];
-    console.log("mensaje del remitente: ", message);
-    //add mark read
-    //add to list
-    //add time sleep
+    
+    list.push(markRead(messageId));
+
     if (/hola/i.test(message)) {
         const textMessage = buildTextMessage(phoneNumber, "hola el bot esta funcionando");
         list.push(textMessage);
